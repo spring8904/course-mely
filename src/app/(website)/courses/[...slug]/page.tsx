5758a722937a6866dee27087adba214bf0597de2
TungTvPh81102 <quaixe121811@gmail.com>
2024-12-28 04:57:38 +0700

    feature/create-course-detail-page

---
+++ src/app/(website)/courses/[...slug]/page.tsx
@@ -0,0 +1,7 @@
+import { CourseDetailView } from '@/sections/courses/view'
+
+const CourseDetaiPage = () => {
+  return <CourseDetailView/>
+}
+
+export default CourseDetaiPage
---
+++ src/sections/courses/_components/course-item.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CourseItemDetail = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CourseItemDetail
