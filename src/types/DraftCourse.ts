dc95adc2d53fce9b20c2d8004be7644c33c54e04
spring8904 <lamnx.dev@gmail.com>
2025-03-07 00:23:31 +0700

    feat: preview draft course

--- src/types/DraftCourse.ts
+++ src/types/DraftCourse.ts
@@ -1,3 +1,5 @@
+import { LessonType } from './Course'
+
 export interface DraftCourse {
   title: string
   status: string
@@ -14,9 +16,26 @@ export interface DraftChapter {
 export interface DraftLesson {
   title: string
   updated_at: Date
-  type: string
-  sample_code: string
-  hints: string[]
-  instruct: null
-  language: string
+  type: LessonType
+  sample_code?: string
+  hints?: string[]
+  instruct?: null
+  language?: string
+  mux_playback_id?: string
+  duration?: number
+  content?: string
+  file_type?: string
+  file_path?: string
+  questions?: DraftQuestion[]
+}
+
+export interface DraftQuestion {
+  answer_type: string
+  question: string
+  answers: DraftAnswer[]
+}
+
+export interface DraftAnswer {
+  answer: string
+  is_correct: boolean
 }
