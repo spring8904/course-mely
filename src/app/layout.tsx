0f442e8c8f5a342945eb45aef570a140c33e6c52
spring8904 <lamnx.dev@gmail.com>
2025-03-09 16:13:31 +0700

    style: update sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import { Metadata } from 'next'
 import { Manrope } from 'next/font/google'
 import { GoogleAnalytics } from '@next/third-parties/google'
-
 import AppProvider from '@/providers/app-provider'
-
 import '@/styles/globals.css'
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL!),
 
-  title: 'CourseMeLy - Nền tảng học trực tuyến hàng đầu Việt Nam',
+  title: {
+    default: 'CourseMeLy - Nền tảng học trực tuyến hàng đầu Việt Nam',
+    template: '%s | CourseMeLy',
+  },
   description:
     '"Nền tảng trực tuyến với đa dạng khóa học từ cơ bản đến nâng cao với nhiều chuyên ngành' +
     'khác nhau, giúp bạn dễ dàng học tập mọi lúc mọi nơi. Được phát triển bởi CourseMeLy"',
